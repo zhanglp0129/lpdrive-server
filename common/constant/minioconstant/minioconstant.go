221d6a88547dad0b1087662263ab3baac736ce14
zhanglp0129 <zhanglp0129@gmail.com>
2025-01-03 22:25:58 +0800

    为上传临时文件添加过期规则

---
+++ common/constant/minioconstant/minioconstant.go
@@ -0,0 +1,10 @@
+package minioconstant
+
+const (
+	// UploadTempLifeCycleID 上传临时文件生命周期id
+	UploadTempLifeCycleID = "upload-temp"
+	// UploadTempPrefix 上传临时目录前缀
+	UploadTempPrefix = "upload-temp/"
+	// UploadExpireDays 上传过期天数
+	UploadExpireDays = 3
+)
